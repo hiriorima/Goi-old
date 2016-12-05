e8b4373dd56041fcc0938cb4022092daf1f83c4c
会津慎弥 <b1013043@fun.ac.jp>
2016-12-05 18:05:54 +0900

    add jbuilder

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,14 @@ Bundler.require(*Rails.groups)
 
 module Goi
 	class Application < Rails::Application
+
+		config.active_record.raise_in_transactional_callbacks = true
+
 		config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
 		config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
+
+		config.middleware.use(Rack::Config) do |env|
+			env['api.tilt.root'] = Rails.root.join 'app', 'views', 'api'
+		end
+		end
 	end
-end
