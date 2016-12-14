422c014ff4b3c127e775af695e8cb77d0de75d0c
会津慎弥 <b1013043@fun.ac.jp>
2016-12-14 22:52:29 +0900

    cooperating with api server

---
+++ app/assets/javascripts/client/actions/WordServerActionCreator.js
@@ -0,0 +1,19 @@
+const
+    ActionTypes = require('../constants/ActionTypes'),
+    AppDispatcher = require('../dispatcher/AppDispatcher');
+
+const WordServerActionCreator = {
+
+  receiveWords : function( words ){
+    AppDispatcher.dispatch({
+      type:ActionTypes.RECOGNITIONED,
+      words : words,
+   });
+ },
+};
+
+module.exports = WordServerActionCreator;
+
+
+
+
